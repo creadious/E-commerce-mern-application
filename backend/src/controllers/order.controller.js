2e286aaa4cf58ea99675f4df45a1b8c7099f6670
rajib-sadhu <rahulrajib.sadhu@gmail.com>
2024-06-23 01:18:00 +0530

    add order payment intent

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -1,4 +1,4 @@
-import mongoose, { ObjectId } from "mongodb";
+import { ObjectId } from "mongodb";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Category } from "../models/category.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
@@ -6,22 +6,97 @@ import { allStatusCode } from "../constants.js";
 import { ApiError } from "../utils/ApiError.js";
 import { APIResponse } from "../utils/ApiResponse.js";
 import { Order } from "../models/order.models.js";
+import { Cart } from "../models/cart.model.js";
 import stripe from "stripe";
 
 const stripeKey = stripe(process.env.PAYMENT_SECRET_KEY);
 
 const createPaymentIntent = asyncHandler(async (req, res) => {
-  const { price } = req.body;
+  const { items } = req.body;
+
+  // const items = [
+  //   {
+  //     _id: "667720de4b54b4eea8e28f31",
+  //   },
+  //   {
+  //     _id: "6676e0353185d26ef066f6f3",
+  //   },
+  // ];
 
   try {
-    if (!price) {
+    if (!Array.isArray(items) || items.length === 0) {
       return res
         .status(allStatusCode.clientError)
-        .json(new ApiError(allStatusCode.clientError, "Please provide price."));
+        .json(
+          new ApiError(
+            allStatusCode.clientError,
+            "Please provide items details"
+          )
+        );
     }
 
-    const amount = parseFloat(price) * 100;
-    console.log(price, amount);
+    const cartItems = [];
+
+    for (let i in items) {
+      const cartDetails = await Cart.aggregate([
+        {
+          $match: { _id: new ObjectId(items[i]._id) },
+        },
+        {
+          $lookup: {
+            from: "products",
+            localField: "productId",
+            foreignField: "_id",
+            as: "productDetails",
+          },
+        },
+        {
+          $addFields: {
+            price: { $arrayElemAt: ["$productDetails.offerPrice", 0] },
+            stock: { $arrayElemAt: ["$productDetails.stock", 0] },
+          },
+        },
+        {
+          $project: {
+            productDetails: 0,
+            userId: 0,
+            productId: 0,
+            createdAt: 0,
+            updatedAt: 0,
+            __v: 0,
+          },
+        },
+      ]);
+      cartItems.push(cartDetails[0]);
+
+      if (!cartDetails[0]) {
+        return res
+          .status(allStatusCode.notFound)
+          .json(
+            new ApiError(
+              allStatusCode.notFound,
+              `Cart id not valid ${items[i]._id}`
+            )
+          );
+      }
+
+      if (cartDetails[0].quantity > cartDetails[0].stock) {
+        return res
+          .status(allStatusCode.notFound)
+          .json(
+            new ApiError(
+              allStatusCode.notFound,
+              `Cart product quantity is out of stock`,
+              { cartId: cartDetails[0]._id }
+            )
+          );
+      }
+    }
+
+    const amount =
+      cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0) *
+      100;
+
     const paymentIntent = await stripeKey.paymentIntents.create({
       amount: amount,
       currency: "inr",
@@ -47,4 +122,6 @@ const createPaymentIntent = asyncHandler(async (req, res) => {
   }
 });
 
-export { createPaymentIntent };
+const paymentComplete = asyncHandler(async (req, res) => {});
+
+export { createPaymentIntent, paymentComplete };
--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { createPaymentIntent } from "../controllers/order.controller.js";
+import {
+  createPaymentIntent,
+  paymentComplete,
+} from "../controllers/order.controller.js";
 
 const router = Router();
 
 router.route("/create-payment-intent").post(createPaymentIntent);
+router.route("/payment").post(verifyJwt, paymentComplete);
 
 export default router;
